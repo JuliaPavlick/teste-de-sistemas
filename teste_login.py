f02abf5cc66a36e827b01356bff8a2e24e25d47d
Júlia Pavlick <juliapavlickk@gmail.com>
2025-08-05 15:30:03 -0300

    .

--- teste_login.py
+++ teste_login.py
@@ -4,23 +4,13 @@ import time
 
 driver = webdriver.Chrome()
 
-driver.get("file:///C:/Users/julia_pavlick/Documents/GitHub/teste_de_sistemas/login.html")   
+driver.get("http://localhost:8080/teste_de_sistemas/login.html")   
    
-
 time.sleep(1)
 
-# Preenche o campo de usuário
-usuario_input = driver.find_element(By.ID, "username")
-usuario_input.send_keys("admin")
-time.sleep(1)
-
-# Preenche o campo de senha
-senha_input = driver.find_element(By.ID, "password")
-senha_input.send_keys("123456")
-
-# Clica no botão de login
-botao_login = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-botao_login.click()
+driver.find_element(By.ID, "username").send_keys("admin")
+driver.find_element(By.ID, "password").send_keys("123456")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
 time.sleep(8)
 if "Cadastro de Cliente" in driver.page_source:
